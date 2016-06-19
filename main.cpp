d0335d64679b112bee8797f64a86fa9e49f43410
EricDoug <eric_doug@126.com>
2016-06-19 10:16:32 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acmer
+//
+//  Created by 刘晓东 on 16/6/19.
+//  Copyright © 2016年 EricDoug. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
